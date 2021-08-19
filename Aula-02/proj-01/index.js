3b7b48c7169bd60834da6f9eef1814a1aaddca39
Fellipe Leal <fellipe.leal@yahoo.com>
2021-08-19 20:15:35 -0300

    att

--- Aula-02/proj-01/index.js
+++ Aula-02/proj-01/index.js
@@ -8,7 +8,7 @@ const filmes = [
     'Velozes e Furiosos',
 ];
 
-// GET   exibe a rota/HOME
+// GET exibe a rota/HOME
 app.get('/', (req, res) => {
     res.send("Bem-Vindos ao site lista de Filmes T3C5-M3");
 });
@@ -18,14 +18,14 @@ app.get('/filmes', (req, res) => {
     res.send(filmes);
 });
 
-//GET do /filmes/id   (Seleciona o filme pelo ID no final do .../id)
+//GET do /filmes/id   (Seleciona o filme pelo ID no final do  .../id)
 app.get('/filmes/:id', (req,res) => {
     const id = req.params.id - 1;
     const filmeSelecionado = filmes[id]
     res.send(filmeSelecionado);
 });
 
-// Abre o Servidor pela porta definida na const em cima.
+// Abre o Servidor pela porta definida na const em cima e apresenta um console.info.
 app.listen(port, () => {
     console.info(`Servidor rodando em: http://localhost:${port}/`)
 });
