bf2cbf1c65632c28f05bed83218abda8f1cf8443
Fellipe Leal <fellipe.leal@yahoo.com>
2021-08-30 23:20:36 -0300

    att

---
+++ projeto-pg/src/components/layout/Cabecalho.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Titulo() {
+    return(
+        <div className='boxTitulo'>
+            <h1>Pontos Turísticos 🗺️</h1>
+        </div>
+    )
+}
---
+++ projeto-pg/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+
+var nome = 'Direitos reservados Leal Code />'
+
+export default function Footer() {
+    return(
+        <>
+            <div className='boxFooter'>
+                <h1>{nome}</h1>
+            </div>
+            <a className='boxGit' href='https://github.com/flealbrl' target='_blank'><button>Git Hub 👾</button></a>
+        </>
+    )}
